7e8c18cb67615a335499f44c05980901b3545707
DESKTOP-6BKNHFU\tarnished <10537@student.puzim.edu.pl>
2023-01-21 21:03:46 +0100

    #userProfile usuwanie konta

--- app/src/main/java/com/inzynierka/komunikat/Activities/ProfileActivity.kt
+++ app/src/main/java/com/inzynierka/komunikat/Activities/ProfileActivity.kt
@@ -1,8 +1,12 @@
 package com.inzynierka.komunikat.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.inzynierka.komunikat.R
 import com.inzynierka.komunikat.classes.User
 import com.squareup.picasso.Picasso
@@ -24,18 +28,46 @@ class ProfileActivity : AppCompatActivity() {
         Picasso.get().load(user?.photoUrl).into(profile_profile_picture)
         profile_user_name.text = user?.name
 
-        //TODO: obsługa przycisków
+        //TODO: dodaj logikę
         profile_firends_list_btn.setOnClickListener {
             Log.d("profileActivity","Click!")
         }
+        //TODO: dodaj logikę
         profile_edit_profile_btn.setOnClickListener {
             Log.d("profileActivity","Click!")
         }
-        profile_delete_account_btn.setOnClickListener {
-            Log.d("profileActivity","Click!")
+        profile_delete_account_btn.setOnClickListener { deleteAccount() }
+
+    }
+
+
+    private fun deleteAccount()
+    {
+        val currentUser = FirebaseAuth.getInstance().currentUser!!
+        val currentUserUid = FirebaseAuth.getInstance().uid
+
+        FirebaseAuth.getInstance().signOut()
+
+        val deletedUsrPhoto = "https://firebasestorage.googleapis.com/v0/b/komunikat-ccfa2.appspot.com/o/images%2Ff4e19f25-15d7-48a3-ad1e-4d15f5e3a3f5?alt=media&token=640d83cf-cbf8-405c-af11-3760a134b6a8"
+        val deletedUser : User = User(currentUserUid!!, "Deleted User", deletedUsrPhoto)
+
+        val userRef = FirebaseDatabase.getInstance().getReference("/users/$currentUserUid")
+        val friendsRef = FirebaseDatabase.getInstance().getReference("/friends/$currentUserUid")
+        val lastMsgRef = FirebaseDatabase.getInstance().getReference("/last_messages/$currentUserUid")
+        val msgRef = FirebaseDatabase.getInstance().getReference("/messages/$currentUserUid")
+
+        userRef.setValue(deletedUser)
+        friendsRef.setValue(null)
+        lastMsgRef.setValue(null)
+        msgRef.setValue(null)
+
+        currentUser.delete().addOnSuccessListener {
+            Toast.makeText(this, "Twoje konto zostało usunięte. Żegnaj :(", Toast.LENGTH_LONG).show()
         }
 
-
+        val intent = Intent(this, RegisterActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
 
     }
 }
